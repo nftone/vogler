47873aa9fa85e075f282bb2e14036cbf7be0ab83
decentraliser <me@decentraliser.dev>
2024-03-26 19:54:04 +0700

    fix indexer

--- indexer/main.py
+++ indexer/main.py
@@ -7,10 +7,15 @@ import threading
 lock = threading.Lock()
 
 
+class APIRequestError(Exception):
+    pass
+
+
 def main():
     with open("./constants/Creations.json", "r", encoding="utf-8") as f:
         creations_json = json.load(f)
         creations = creations_json["creations"]
+
         updated_creations = []
         current_timestamp = datetime.now().isoformat()
         updated_creation_index = 0
@@ -18,9 +23,16 @@ def main():
         for creation in creations:
             updated_creation_index += 1
             print(f"Processing creation {updated_creation_index}/{len(creations)}")
-            owner = find_owner_by_tx_hash(creation["h"])
-            output_creation = {"owner": owner, "name": creation["name"], "image": creation["image"]}
-            updated_creations.append(output_creation)
+            try:
+                owner = find_owner_by_tx_hash(creation["h"])
+                output_creation = {
+                    "owner": owner,
+                    "name": creation["name"],
+                    "image": creation["image"],
+                }
+                updated_creations.append(output_creation)
+            except APIRequestError as e:
+                print(f"Error: {e}")
 
         output_dict = {"last_update": current_timestamp, "creations": updated_creations}
 
@@ -30,42 +42,49 @@ def main():
 
 
 def find_owner_by_tx_hash(tx_hash):
-    owner, vout = get_owner_or_vout_hash(tx_hash)
-    if owner:
-        return owner
-    return find_owner_by_tx_hash(vout)
-
-
-def get_owner_or_vout_hash(tx_hash):
-    tx = get_tx(tx_hash)
-    if tx is None:
-        return None, None
+    tx = get_tx_by_hash(tx_hash)
 
     vout = tx["vout"]
-    spent_tx = None
 
     for v in vout:
-        if v.get("spent"):
-            spent_tx = v
-            break
+        if v.get("value", 0) > 0:
+            return find_owner_by_address(v.get("scriptpubkey_address"))
 
-    if spent_tx:
-        print(spent_tx, "spent_tx")
-        return None, spent_tx["spentTxId"]
-
-    print(vout[0], "vout[0]")
-    return vout[0]["scriptpubkey_address"], None
+    return vout[0]["scriptpubkey_address"]
 
 
-def get_tx(tx_hash):
+def find_owner_by_address(tx_address):
+    potential_owner_txs = get_address_transactions(tx_address)
+
+    for tx in potential_owner_txs:
+        for vout in tx["vout"]:
+            if (
+                vout.get("value", 0) > 0
+                and vout.get("scriptpubkey_address") != tx_address
+            ):
+                return find_owner_by_address(vout["scriptpubkey_address"])
+
+    return tx_address
+
+
+def get_tx_by_hash(tx_hash):
     sleep(0.4)
     url = f"https://blockstream.info/api/tx/{tx_hash}"
     response = requests.get(url, timeout=10)
     if response.status_code == 200:
         return response.json()
 
-    print(f"Error: API request failed with status code {response.status_code}")
-    return None
+    raise APIRequestError(f"API request failed with status code {response.status_code}")
+
+
+def get_address_transactions(address):
+    sleep(0.4)
+    url = f"https://blockstream.info/api/address/{address}/txs"
+    response = requests.get(url, timeout=10)
+    if response.status_code == 200:
+        return response.json()
+
+    raise APIRequestError(f"API request failed with status code {response.status_code}")
 
 
 main()
