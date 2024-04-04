7eaadffb173deecfd9554d1a3cdc39888f114f35
decentraliser <me@decentraliser.dev>
2024-04-04 16:23:44 +0700

    Add introduction text to home page

--- front-end/src/Home.vue
+++ front-end/src/Home.vue
@@ -14,6 +14,7 @@
     <h2>Something went wrong</h2>
     <p>{{ creationsErrorMessage }}</p>
   </div>
+
   <div v-else>
     <div class="creations-grid">
       <CreationTile
@@ -21,6 +22,59 @@
         :key="`creation-tile-${creation.h}`"
         :creation="creation"
       />
+    </div>
+    <div class="introduction">
+      <p>
+        On this website I present my works. They deal with questions about philosophy, information
+        theory, art, law and economy.
+      </p>
+      <p>
+        My works are immaterial. Every work is a file and has been electronically signed in
+        compliance with the German Digital Signature Act. The electronic signature is equal to a
+        written signature on a physical document. Additionally every signature contains a time
+        stamp, proving that the file existed and was known to the signee at a specific time.
+      </p>
+      <p>
+        The titles of the works are not arbitrary, random or time based. Instead they are SHA256
+        hash values of the works' data. Every work is published under a license which transforms its
+        usage rights using the bitcoin infrastructure into a immaterial good. The granting of the
+        right to transfer the good is exclusive, that is legally limited to only one licensee. The
+        immaterial good has following attributes:
+      </p>
+      <p style="margin-bottom: 0">
+        In spite of its immaterial nature, the good is legally and technically limited and
+        independently transferable respectively tradable.
+      </p>
+      <p style="margin: 0">
+        The authenticity of the signature and the signature date of the work, the good is based on,
+        can be proven beyond a reasonable doubt.
+      </p>
+      <p style="margin: 0">
+        The acquisition of the good comes with more usage rights (e.g. the right to copy and to
+        publish the good) than the acquisition of a material good.
+      </p>
+      <p style="margin: 0">
+        The “ownership” of the immaterial good and the entitlement to the connected usage rights can
+        be proven by the owner beyond a reasonable doubt.
+      </p>
+      <p style="margin: 0">
+        The good can be securely and instantaneously sold or transferred worldwide with minimal
+        transaction costs.
+      </p>
+      <p style="margin: 0">
+        If the good is paid with Bitcoins it can be securely sold worldwide without the need for an
+        escrow service.
+      </p>
+      <p>
+        Hence the good has many attributes of a material good, but also additional attributes, like
+        the possibility for a proof of authenticity of the signature beyond a reasonable doubt,
+        which is impossible or very difficult for material works. Besides the good can also be based
+        on a immaterial work like a piece of music.
+      </p>
+      <p>
+        Because of the technical provisions of the license a list of my works can be found in the
+        bitcoin block chain.
+      </p>
     </div>
   </div>
 </template>
@@ -59,7 +113,14 @@ onMounted(async () => {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
   column-gap: 22px;
+}
+
+.creations-grid,
+.introduction {
   max-width: 1760px;
-  margin-top: 1rem;
+}
+
+.introduction {
+  margin-top: 3rem;
 }
 </style>
