b6094d3d8e9eb043ff54de23b19e34785dc3fc46
decentraliser <me@decentraliser.dev>
2024-04-04 13:04:23 +0700

    bootstrap Home component

--- front-end/src/Home.vue
+++ front-end/src/Home.vue
@@ -1,3 +1,48 @@
 <template>
-  <RouterLink to="/verifier">Verifier</RouterLink>
+  <div class="home-menu no-select">
+    <h1>Stephan Vogler Artworks (2015)</h1>
+    <RouterLink to="/verifier" :custom="true">
+      <h2 class="address-verifier-link">Address Verifier</h2>
+    </RouterLink>
+  </div>
+
+  <div v-if="loadingCreations">Loading...</div>
+
+  <div v-else-if="creationsErrorMessage">
+    <h2>Something went wrong</h2>
+    <p>{{ creationsErrorMessage }}</p>
+  </div>
+  <div v-else>
+    {{ creations }}
+  </div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue'
+import useCreations from './composables/useCreations'
+
+const {
+  creations, //
+  creationsErrorMessage,
+  loadingCreations,
+  refreshCreations
+} = useCreations()
+
+onMounted(async () => {
+  await refreshCreations({ refreshIfEmpty: true })
+})
+</script>
+
+<style>
+.home-menu {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: end;
+}
+
+.address-verifier-link {
+  cursor: pointer;
+  text-align: right;
+  padding-bottom: 3px;
+}
+</style>
