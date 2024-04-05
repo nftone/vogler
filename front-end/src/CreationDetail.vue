d01b0d9d00f3d721ffa2b546b1aa822dbb372ab4
decentraliser <me@decentraliser.dev>
2024-04-05 11:36:05 +0700

    finalize creation details properties

--- front-end/src/CreationDetail.vue
+++ front-end/src/CreationDetail.vue
@@ -30,9 +30,29 @@
 
         <div class="properties">
           <p style="margin-top: 0">Description: {{ creation.description }}</p>
-          <p>Hash/File: {{ fileHash }}</p>
-          <p>Signed: {{ creation.signatureDate }} CEST</p>
-          <p>Inscribed: {{ creation.inscriptionDate }} CEST</p>
+          <p>
+            Hash/File:
+            <a v-if="creation.fileUrl.startsWith('http')" :href="creation.fileUrl" target="_blank">
+              {{ fileHash }}
+            </a>
+            <template v-else>{{ fileHash }}</template>
+          </p>
+          <p>
+            Signed: {{ creation.signatureDate }} CEST (<a
+              :href="creation.signatureUrl"
+              target="_blank"
+            >
+              {{ creation.signatureUrl }} </a
+            >)
+          </p>
+          <p>
+            Inscribed: {{ creation.inscriptionDate }} CEST (<a
+              :href="inscriptionUrl"
+              target="_blank"
+            >
+              {{ inscriptionUrl }} </a
+            >)
+          </p>
           <p>Format: {{ creation.format }}</p>
           <p>Size: {{ creation.size }}</p>
           <p>
@@ -41,8 +61,19 @@
               {{ creation.license }}
             </a>
           </p>
-          <p>Owner: {{ creation.owner }}</p>
-          <p>Owner contact: {{ creation.ownerContact }}</p>
+          <p>
+            Owner: <a :href="ownerUrl" target="_blank">{{ creation.owner }}</a>
+          </p>
+          <p>
+            Owner contact:
+            <a
+              v-if="creation.ownerContact.startsWith('http')"
+              :href="creation.ownerContact"
+              target="_blank"
+              >{{ creation.ownerContact }}</a
+            >
+            <template v-else>{{ creation.ownerContact }}</template>
+          </p>
         </div>
       </div>
     </div>
@@ -77,6 +108,14 @@ const fileHash = computed(() => {
   if (!creation.value) return ''
   const fileName = creation.value.signatureUrl.split('/').pop()
   return fileName.split('.')[0]
+})
+
+const inscriptionUrl = computed(() => {
+  return `https://www.blockchain.com/explorer/transactions/btc/${creation.value.h}`
+})
+
+const ownerUrl = computed(() => {
+  return `https://www.blockchain.com/explorer/addresses/btc/${creation.value.owner}`
 })
 
 onMounted(async () => {
